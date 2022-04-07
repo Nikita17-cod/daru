bf415543d2ff3eeb514ed8d7ca1b0164a0499eb6
Nikita Koshik <nikitakoshik064@gmail.com>
2022-04-07 19:45:33 +0300

    07.04.2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,6 @@
-
-$(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
-  });
-
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+        $('.header__burger, .header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
 });
